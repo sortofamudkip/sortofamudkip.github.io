2e47cf8fa720e8ff954d5b6aa66454f2fe728701
Wishyut Pitawanik <wishyutp0328@gmail.com>
2018-04-06 04:04:32 +0800

    new index page!

---
+++ DSA_navbar.js
@@ -0,0 +1,68 @@
+var output =
+	'<div class = "navbar">' +
+		'<span class="nav"><a href="index.htm"><span class="lang eng">Home</span><span class="lang tw">首頁</span></a></span>' +
+		'<span class="nav"><a href="DSA_home.htm"><span class="lang eng">Intro</span><span class="lang tw">簡介</span></a></span>' +
+		'<span class="nav"><a href="asym.htm"><span class="lang eng">Asymptotic Analysis</span><span class="lang tw">時間複雜分析</span></a></span>' +
+		'<span class="nav"><a href="#stack"><span class="lang eng">Stack &amp; Queue</span><span class="lang tw">堆疊 &amp; 佇列</span></a></span>' +
+		'<span class="nav"><a href="#LL"><span class="lang eng">Linked List</span><span class="lang tw">連結串列</span></a></span>' +
+
+		'<span class="nav"><a href="#sort"><span class="lang eng">Sorting</span><span class="lang tw">排序</span></a></span>' +
+		'<span class="nav"><a href="#sort"><span class="lang eng">C++\'s STL</span><span class="lang tw">C++ 標準庫</span></a></span>' +
+
+		// '<span class="dropdown">' +
+		//   '<button class="dropbtn">Dropdown '+
+		//     '<i class="fa fa-caret-down"></i>'+
+		//   '</button>'+
+		//   '<div class="dropdown-content" style="z-index:2;">'+
+		//     '<a href="asym.htm">Link 1</a>'+
+		//     '<a href="#">Link 2</a>'+
+		//     '<a href="#">Link 3</a>'+
+		//   '</div>' +
+		// '</span>' +
+
+		'<span class ="nav" style="float:right">' +
+			'<button class="nav" type="button" onclick="openNav()">' +
+				'<span class="lang eng">About</span> <span class="lang tw">關於</span>' + 
+			'</button>' +
+		'</span>' +
+		'<span class ="nav" style="float:right">' +
+			'<button class="nav" type="button" onclick="">' +
+				'<span class="lang eng">Chinese (WIP!)</span> <span class="lang tw">中文</span>' +
+			'</button>' +
+		'</span>' +
+		'<span class="nav" style="float:right"> ' +
+			'<button class = "nav" type="button" onclick="switch_lang(\'eng\')">' +
+				'<span class="lang eng">English</span> <span class="lang tw">英文</span>' +
+		  	'</button>' +
+		'</span>' +
+	'</div>'	+
+	'<div id="mySidenav" class="sidenav"> \
+	  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a> \
+	  <div style="padding-left: 20px; word-wrap: normal;"> \
+	  	<h3>About the dev</h3> \
+	  	<img src="kippp.jpg"> \
+	  	<p> \
+	  		Jake (馮永輝) is a second year CS student at NTU.  \
+	  	</p> \
+	  	<p>\
+	  		He is also probably depressed.\
+	  	</p>\
+	  	<p>\
+	  		He also wants a job/internship. Maybe you should help him out.\
+	  	</p>\
+	    <div><a href="https://fthmb.tqn.com/hIWmQMsJPN3j_eFPrSPVVse_52Q=/2121x1414/filters:fill(auto,1)/GettyImages-185002046-5772f4153df78cb62ce1ad69.jpg">Help me out</a></div>\
+	    <div><a href="https://www.wikihow.com/Be-Generous">Don\'t help me out</a></div>\
+	  </div>\
+	</div>\
+\
+	<script type="text/javascript">\
+		function openNav() {\
+		    document.getElementById("mySidenav").style.width = "250px";\
+		}\
+		function closeNav() {\
+		    document.getElementById("mySidenav").style.width = "0px";\
+		}\
+	</script>\
+	';
+
+document.write(output);
